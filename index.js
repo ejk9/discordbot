f134ff57742b375afa9ec966f3781098d429ac84
Utaha-Kasumigod <57812344+Utaha-Kasumigod@users.noreply.github.com>
2020-02-22 14:09:36 -0700

    newton math

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //console.log("Hello, World!");
 
 const Discord =  require('discord.js');
+const newton = require('newtonmath.js');
 const client = new Discord.Client();
 
 const token = 'NjgwODE1ODk0NDE4Njg1OTYy.XlGMYA._QdAyit7mUp1uaC_X_S5mzrXVB8';
@@ -8,6 +9,12 @@ const token = 'NjgwODE1ODk0NDE4Njg1OTYy.XlGMYA._QdAyit7mUp1uaC_X_S5mzrXVB8';
 client.on('ready', () =>{
     console.log('This bot is online');
 })
+
+client.on('message', msg=>{
+    
+})
+
+
 
 client.login(token);
 
