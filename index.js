b584c76ad7ddf2781424aecb5a2709f3fa00956f
Utaha-Kasumigod <57812344+Utaha-Kasumigod@users.noreply.github.com>
2020-02-22 13:16:41 -0700

    added bot becoming online

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+//console.log("Hello, World!");
+
+const Discord =  require('discord.js');
+const client = new Discord.Client();
+
+const token = 'NjgwODE1ODk0NDE4Njg1OTYy.XlGKSw.wrnuunc4AQpHireuvF1c-h05J_8';
+
+client.on('ready', () =>{
+    console.log('This bot is online');
+})
+
+client.login(token);
+
+
+
